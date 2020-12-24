532a687390015bc8431ce5eb68b094e44cda806d
Yusuke Uchida <fantomyuu0623@gmail.com>
2020-12-24 23:23:54 +0900

    d183.py: changed method

--- atcoder/abc/d183.py
+++ atcoder/abc/d183.py
@@ -1,46 +1,25 @@
 def main():
     N, W = list(map(int, input().split()))
-    ll_s = []
-    ll_t = []
+    time_tot = int(2 * 1e5 + 1)
+    T = [0] * (time_tot)
 
+    # record
     for _ in range(N):
-        s, t, p = tuple(map(int, input().split()))
-        ll_s.append((s, p))
-        ll_t.append((t, p))
+        s, t, p = map(int, input().split())
+        T[s] = T[s] + p
+        T[t] = T[t] - p
 
-    ll_s.sort()
-    ll_t.sort()
+    # simulate
+    simulate = [0]
+    for i in range(len(T)):
+        amount = simulate[-1] + T[i]
+        if amount > W:
+            print("No")
+            return -1
+        simulate.append(amount)
 
-    ammt_now = 0
-    idx_s = 0
-    idx_t = 0
-    for _ in range(2 * N):
-        # print(ammt_now)
-        # print("idx ", idx_s, idx_t)
-        # print("time ", ll_s[idx_s][0], ll_t[idx_t][1])
-        if ll_s[idx_s][0] < ll_t[idx_t][0]:
-            ammt_now += ll_s[idx_s][1]
-            idx_s += 1
-            # print("add ", idx_s)
-            if ammt_now > W:
-                # print("last amount ", ammt_now)
-                print("No")
-                return -1
-        else:
-            ammt_now -= ll_t[idx_t][1]
-            idx_t += 1
-            # print("sub ", idx_t)
-
-        # 終了条件
-        if idx_t == N or idx_s == N:
-            break
-
+    # result
     print("Yes")
-
-
-def test():
-    t = tuple(map(int, input().split()))
-    print(type(t))
 
 
 if __name__ == "__main__":
