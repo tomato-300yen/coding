6b47de62218ec67bb04e4a567fcde1732a058b83
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-02-21 23:02:56 +0900

    abc: c122

--- atcoder/abc/c122.py
+++ atcoder/abc/c122.py
@@ -1,5 +1,10 @@
 N, Q = map(int, input().split())
 S = input()
 LR = [map(int, input().split()) for _ in range(Q)]
+arr = [0 for _ in range(N)]
+for i in range(1, N):
+    arr[i] = arr[i - 1]
+    if S[i - 1 : i + 1] == "AC":
+        arr[i] += 1
 for left, right in LR:
-    print(S[left - 1 : right].count("AC"))
+    print(arr[right - 1] - arr[left - 1])
