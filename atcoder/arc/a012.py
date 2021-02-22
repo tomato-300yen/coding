881ba16437d98b29c5878748d613c97ab7d85b79
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-02-22 12:14:42 +0900

    arc: a012

---
+++ atcoder/arc/a012.py
@@ -0,0 +1,11 @@
+day = input()
+dict_weekday = {
+    "Sunday": 0,
+    "Monday": 5,
+    "Tuesday": 4,
+    "Wednesday": 3,
+    "Thursday": 2,
+    "Friday": 1,
+    "Saturday": 0,
+}
+print(dict_weekday[day])
