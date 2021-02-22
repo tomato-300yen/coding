799e99ee694bd2aba05d94dd3336c827a7162945
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-02-22 10:55:16 +0900

    abc: b098

--- atcoder/abc/b098.py
+++ atcoder/abc/b098.py
@@ -1,6 +1,6 @@
 N = int(input())
 S = input()
-max_ = 0
+ans = 0
 for i in range(N):
-    max_ = max(max_, len(set(S[:i]) & set(S[i:])))
-print(max_)
+    ans = max(ans, len(set(S[:i]) & set(S[i:])))
+print(ans)
