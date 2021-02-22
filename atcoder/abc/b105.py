266fae6bac3d464a7465265d0e6a05e45d10e629
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-02-22 11:44:41 +0900

    abc: b105

--- atcoder/abc/b105.py
+++ atcoder/abc/b105.py
@@ -1,15 +1,7 @@
-def main():
-    N = int(input())
-    ans = 0
-    for i in range(1, N + 1, 2):
-        count = 1
-        for j in range(3, i + 1, 2):
-            if i % j == 0:
-                count += 1
-        if count == 8:
-            ans += 1
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
+N = int(input())
+for i in range(N // 4 + 1):
+    if (N - i * 4) % 7 == 0:
+        print("Yes")
+        break
+else:
+    print("No")
