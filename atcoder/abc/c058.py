fb0a4fd96ce80c075a4524f6c080fed9d12d0f64
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-03-01 15:23:36 +0900

    abc: c058 modified

--- atcoder/abc/c058.py
+++ atcoder/abc/c058.py
@@ -1,15 +1,7 @@
 from collections import Counter
 N = int(input())
 S = [Counter(input()) for _ in range(N)]
-ans_dict = S[0]
-# count
+ans_counter = S[0]
 for s in S:
-    tmp_dict = dict()
-    for key in ans_dict.keys() & s.keys():
-        tmp_dict[key] = min(ans_dict[key], s[key])
-    ans_dict = tmp_dict
-# 文字列を作成
-ans = ""
-for s in sorted(ans_dict.keys()):
-    ans = ans + s * ans_dict[s]
-print(ans)
+    ans_counter &= s
+print("".join(sorted(ans_counter.elements())))
