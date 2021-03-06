2391b6e309aca72918d718a33c95ee26c2afccf8
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-03-06 17:50:14 +0900

    abc: b122, new method

--- atcoder/abc/b122.py
+++ atcoder/abc/b122.py
@@ -1,10 +1,6 @@
-S = input()
-count = 0
-ans = 0
-for s in S:
-    if s == "A" or s == "T" or s == "G" or s == "C":
-        count += 1
-    else:
-        count = 0
-    ans = max(ans, count)
-print(ans)
+import re
+print(
+    max(
+        map(len, re.findall("[ACGT]*", input()))
+    )
+)
