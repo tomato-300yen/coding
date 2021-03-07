776a01c92b3c0156108a7bef1ad9214caef33a39
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-03-07 13:29:37 +0900

    abc: b104, new method

--- atcoder/abc/b104.py
+++ atcoder/abc/b104.py
@@ -1,7 +1,12 @@
-S = input()
-cond = (
-    (S[0] == "A")
-    and (list(S[2:-1]).count("C") == 1)
-    and S[1:].replace("C", "").islower()
-)
-print("AC" if cond else "WA")
+# S = input()
+# cond = (
+#     (S[0] == "A")
+#     and (list(S[2:-1]).count("C") == 1)
+#     and S[1:].replace("C", "").islower()
+# )
+# print("AC" if cond else "WA")
+import re
+if re.match("^A[a-z]+C[a-z]+$", input()):
+    print("AC")
+else:
+    print("WA")
