f19c7ee0a87d0bae3662357b17d12d4b189fa9c3
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-03-07 14:10:18 +0900

    abc: b017, new method

--- atcoder/abc/b017.py
+++ atcoder/abc/b017.py
@@ -1,20 +1,22 @@
-X = input()
-i = 0
-while i < len(X):
-    if X[i] == "c":
-        if X[i + 1] != "h":
-            print("NO")
-            break
-        i += 1
-    elif X[i] == "o":
-        pass
-    elif X[i] == "k":
-        pass
-    elif X[i] == "u":
-        pass
-    else:
-        print("NO")
-        break
-    i += 1
-else:
-    print("YES")
+# X = input()
+# i = 0
+# while i < len(X):
+#     if X[i] == "c":
+#         if X[i + 1] != "h":
+#             print("NO")
+#             break
+#         i += 1
+#     elif X[i] == "o":
+#         pass
+#     elif X[i] == "k":
+#         pass
+#     elif X[i] == "u":
+#         pass
+#     else:
+#         print("NO")
+#         break
+#     i += 1
+# else:
+#     print("YES")
+import re
+print("YES" if re.match("^(ch|o|k|u)*$", input()) else "NO")
