da1fe19290bdcca7e6d0fa33161237601bfa39f1
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-03-28 00:26:00 +0900

    abc: e197

--- atcoder/abc/e197.py
+++ atcoder/abc/e197.py
@@ -1,50 +1,22 @@
 from itertools import groupby
+
 N = int(input())
 XC = [list(map(int, input().split())) for _ in range(N)]
 XC = sorted(XC, key=lambda ll: (ll[1]))
-last = 0
+last_left = 0
+last_right = 0
 ans = 0
-move_list = []
-lr_list = []
+a, b = 0, 0
 for k, g in groupby(XC, key=lambda ll: ll[1]):
     g = list(map(lambda ll: ll[0], g))
     left = min(g)
     right = max(g)
-    move_to_left = abs(last - left)
-    move_to_right = abs(last - right)
-    lr_list.append((left, right))
-    move_list.append((move_to_left, move_to_right))
-    if left <= last <= right:
-        if move_to_left > move_to_right:
-            ans += right - left + move_to_right
-            last = left
-        else:
-            ans += right - left + move_to_left
-            last = right
-    else:
-        if move_to_left > move_to_right:
-            ans += move_to_left
-            last = left
-        else:
-            ans += move_to_right
-            last = right
-
-# for i in range(len(lr_list)):
-#     left, right = lr_list[i]
-#     move_to_left, move_to_right = move_list[i]
-#     if left <= last <= right:
-#         if move_to_left > move_to_right:
-#             ans += right - left + move_to_right
-#             last = left
-#         else:
-#             ans += right - left + move_to_left
-#             last = right
-#     else:
-#         if move_to_left > move_to_right:
-#             ans += move_to_left
-#             last = left
-#         else:
-#             ans += move_to_right
-#             last = right
-ans += last
-print(ans)
+    length = right - left
+    a, b = (
+        length + min(abs(right - last_left) + a, abs(right - last_right) + b),
+        length + min(abs(left - last_left) + a, abs(left - last_right) + b),
+    )
+    last_right = right
+    last_left = left
+a, b = a + abs(last_left), b + abs(last_right)
+print(min(a, b))
