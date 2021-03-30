ca530ae834ebfd7a2851859c9e932a573d414c61
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-03-31 01:45:59 +0900

    abc: d051, dijkstra

---
+++ atcoder/abc/d051.py
@@ -0,0 +1,65 @@
+import heapq
+
+
+class GraphList:
+    def __init__(self, maxsize=10 ** 6):
+        """
+        nodes : 0, 1, 2, ... , n-1
+        edges[node_from] describes edges from node_from
+        Edge type(edge_type) defines the type of element of edges  :
+            'cost_mod'   -- cost*n + node_to
+            'unweighted' -- node_to
+        """
+        self._n = maxsize  # number of nodes
+        self._edges = [[] for _ in range(self._n)]  # adjacency list
+
+    def add_edge(self, a, b, cost=1, directed=False):
+        """Add edge
+        directed   : a ---> b (cost)
+        undirected : a <--> b (cost)
+        """
+        self._edges[a].append(cost * self._n + b)
+        if not directed:
+            self._edges[b].append(cost * self._n + a)
+
+    def dijkstra(self, node_start, initval=float("inf")):
+        """
+        Return shoretet distance from node_start
+        """
+        assert 0 <= node_start < self._n
+        list_dijkstra = [initval] * self._n
+        heapnode_dist = [node_start]
+        while heapnode_dist:
+            # Where to visit
+            node_now = heapq.heappop(heapnode_dist)
+            cost, node_to = divmod(node_now, self._n)
+            # Continue if visited
+            if list_dijkstra[node_to] != initval:
+                continue
+            # Visit
+            list_dijkstra[node_to] = cost
+
+            # Update cost of nodes adjacent to the node(node_to).
+            for edge_nxt in self._edges[node_to]:
+                cost_nxt, node_nxt = divmod(edge_nxt, self._n)
+                # continue if visited
+                if list_dijkstra[node_nxt] != initval:
+                    continue
+                # Update cost
+                heapq.heappush(heapnode_dist, (cost + cost_nxt) * self._n + node_nxt)
+        return list_dijkstra
+
+
+N, M = map(int, input().split())
+ABC = [list(map(int, input().split())) for _ in range(M)]
+g = GraphList(N)
+for a, b, c in ABC:
+    g.add_edge(a - 1, b - 1, c)
+dist_list = []
+for i in range(N):
+    dist_list.append(g.dijkstra(i))
+ans = 0
+for a, b, c in ABC:
+    if dist_list[a - 1][b - 1] < c:
+        ans += 1
+print(ans)
