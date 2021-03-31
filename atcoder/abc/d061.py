d83d6b49d04bb11baeee000297860caddeb420d3
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-03-31 20:00:55 +0900

    abc: d061, WA

--- atcoder/abc/d061.py
+++ atcoder/abc/d061.py
@@ -1,46 +1,57 @@
-class GraphMat:
+from collections import deque
+
+
+class GraphList:
     def __init__(self, maxsize=10 ** 6):
         """
         nodes : 0, 1, 2, ... , n-1
-        edges[node_i][node_j] describes cost between node_i and node_j
+        edges[node_from] describes edges from node_from
+        Edge type(edge_type) defines the type of element of edges  :
+            'cost_mod'   -- cost*n + node_to
+            'unweighted' -- node_to
         """
         self._n = maxsize  # number of nodes
-        self._edges = [
-            [0 for _ in range(self._n)] for _ in range(self._n)
-        ]  # adjacency matrix
+        self._edges = [[] for _ in range(self._n)]  # adjacency list
 
-    def add_edge(self, a, b, cost, directed=False):
+    def add_edge(self, a, b, cost=1, directed=False):
         """Add edge
         directed   : a ---> b (cost)
         undirected : a <--> b (cost)
         """
-        self._edges[a][b] = cost
+        self._edges[a].append(cost * self._n + b)
         if not directed:
-            self._edges[b][a] = cost
+            self._edges[b].append(cost * self._n + a)
 
-    def bellman_ford(self, start=None, initval=float("inf")):
+    def bellman_ford(self, node_start, initval=float("inf")):
         """
-        Return distance matrix
+        Negative cost can be treated.
+        Return shoretet distance to each nodes from node_start.
+        None is returned if negative loop was found.
         """
-        bf_list = [
-            [0 if i == j else initval if el == 0 else el for i, el in enumerate(line)]
-            for j, line in enumerate(self._edges)
-        ]
-        for k in range(self._n):
-            for i in range(self._n):
-                if start is not None and start != i:
-                    continue
-                for j in range(self._n):
-                    bf_list[i][j] = max(bf_list[i][j], bf_list[i][k] + bf_list[k][j])
-        return bf_list
+        assert 0 <= node_start < self._n
+        list_bellman = [initval] * self._n
+        list_bellman[node_start] = 0
+        queue = deque([node_start])
+        count = 0
+        while queue:
+            node_now = queue.popleft()
+            for edge_nxt in self._edges[node_now]:
+                cost_nxt, node_nxt = divmod(edge_nxt, self._n)
+                new_cost = list_bellman[node_now] + cost_nxt
+                if list_bellman[node_nxt] > new_cost:
+                    list_bellman[node_nxt] = new_cost
+                    queue.append(node_nxt)
+                    # negative loop was found
+                    if count == self._n:
+                        return None
+                    count += 1
+        return list_bellman
 
 
 N, M = map(int, input().split())
 ABC = [list(map(int, input().split())) for _ in range(M)]
-g = GraphMat(N)
+g = GraphList(N)
 for a, b, c in ABC:
-    g.add_edge(a - 1, b - 1, c, directed=True)
-bf_list = g.bellman_ford(start=0, initval=0)
-print(*g._edges, sep="\n")
-print(*bf_list, sep="\n")
-print(bf_list[0][-1])
+    g.add_edge(a - 1, b - 1, -c, directed=True)
+bf_list = g.bellman_ford(node_start=0)
+print(-bf_list[-1] if bf_list else "inf")
